fca6179d2a0fff9c892c48ce04c8a8db14b03939
Abhinav Sudhakar <28253243+AbhinavSdr@users.noreply.github.com>
2025-01-22 20:12:12 +0530

    initial upload

---
+++ src/test/java/com/ust/capstone/role_service/RoleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.capstone.role_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
